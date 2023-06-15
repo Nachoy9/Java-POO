5b7177921e87d91cb55d47047eb96278bb5ff0cf
Ignacio Cholaquides <nachoy9@hotmail.com>
2023-06-15 20:21:01 -0300

    Java POO Ejercicio Grupal 1

--- src/ActividadExtra1/Principal/Principal.java
+++ src/ActividadExtra1/Principal/Principal.java
@@ -1,3 +1,18 @@
+/*
+Crear una clase "Vehículo" con atributos como "marca", "modelo", "año" y "tipo" (En tipo, vamos a indicar si es un
+automóvil, una motocicleta, o una bicicleta). El vehículo debe tener los métodos de moverse y frenar:
+* Moverse: Es la cantidad de metros que avanzará por segundo.
+* Frenar: Dejará de avanzar y se frenará 2 metros más adelante. En el caso de la bicicleta, se frenará y no avanzará
+más metros.
+Referencias:
+- Un automóvil avanza 3 metros por segundo.
+- Una motocicleta avanza 2 metros por segundo.
+- Una bicicleta avanza 1 metro por segundo.
+Una vez realizado el programa, crear 3 vehículos diferentes (entre todos elegir que tipo de vehículo es), y mostrar
+por pantalla cuanto avanzan si se mueve durante 5 segundos, durante 10 segundos y durante 1 minuto. Luego mostrar
+que vehículo logró llegar más lejos luego de frenar, tras avanzar durante 5 minutos, y cual fue la cantidad de metros que avanzó.
+ */
+
 package ActividadExtra1.Principal;
 
 import java.util.Scanner;
